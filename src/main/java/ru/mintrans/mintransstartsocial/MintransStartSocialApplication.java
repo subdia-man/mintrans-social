3f7ec222c362aa382661af060653ade6af4053b9
subdia <subdia.subdia@gmail.com>
2024-02-24 01:24:16 +0300

    Initial commit

---
+++ src/main/java/ru/mintrans/mintransstartsocial/MintransStartSocialApplication.java
@@ -0,0 +1,13 @@
+package ru.mintrans.mintransstartsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintransStartSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MintransStartSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mintrans/mintransstartsocial/MintransStartSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mintrans.mintransstartsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintransStartSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
