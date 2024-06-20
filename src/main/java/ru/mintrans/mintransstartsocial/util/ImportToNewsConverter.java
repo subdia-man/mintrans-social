0acf58059e7ba7f6f867536599208deb63058b85
subdia <subdia.subdia@qgmail.com>
2024-06-20 17:01:17 +0300

    Started with ImportToNews utility

---
+++ src/main/java/ru/mintrans/mintransstartsocial/util/ImportToNewsConverter.java
@@ -0,0 +1,16 @@
+package ru.mintrans.mintransstartsocial.util;
+
+import ru.mintrans.mintransstartsocial.model.Import;
+import ru.mintrans.mintransstartsocial.model.News;
+
+import java.io.IOException;
+
+public class ImportToNewsConverter {
+    private Import utilImport = new Import();
+    private News utilNews = new News();
+
+    public News ConvertImportToNews (Import incomingImport) throws IOException {
+
+        return utilNews;
+    }
+}
