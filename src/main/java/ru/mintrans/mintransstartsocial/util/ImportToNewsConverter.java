0720f3deae6cfdb137371a7916a30482e41e9ae5
subdia <subdia.subdia@gmail.com>
2024-06-21 21:47:02 +0300

    Added Import entity

--- src/main/java/ru/mintrans/mintransstartsocial/util/ImportToNewsConverter.java
+++ src/main/java/ru/mintrans/mintransstartsocial/util/ImportToNewsConverter.java
@@ -4,13 +4,41 @@ import ru.mintrans.mintransstartsocial.model.Import;
 import ru.mintrans.mintransstartsocial.model.News;
 
 import java.io.IOException;
+import java.util.Date;
 
 public class ImportToNewsConverter {
     private Import utilImport = new Import();
     private News utilNews = new News();
 
     public News ConvertImportToNews (Import incomingImport) throws IOException {
+        // disassembly incomingImport to from and where references
+        String importFrom = GetImportFromReference(incomingImport);
+        String importWhere = GetImportWhereReference(incomingImport);
+        // save date of import
+        Date doa = incomingImport.getDoi();
+        // take data from where, segregate header and data
+        String headerOfIncomingImport = GetHeaderFromImport(importFrom);
+        String bodyOfIncomingImport = GetBodyFromImport(importFrom);
+        // construct News
+        News incomingNewsFromImport = new News(headerOfIncomingImport, bodyOfIncomingImport, doa);
+        return incomingNewsFromImport;
+    }
 
-        return utilNews;
+    public String GetImportFromReference (Import incomingImport) {
+        return incomingImport.getFrom();
+    }
+
+    public String GetImportWhereReference (Import incomingImport) {
+        return incomingImport.getWhere();
+    }
+
+    public String GetHeaderFromImport (String importFrom) {
+        String header = "Some header";
+        return header;
+    }
+
+    public String GetBodyFromImport (String importFrom) {
+        String body = "Some body";
+        return body;
     }
 }
